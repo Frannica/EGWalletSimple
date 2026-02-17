18dad95d547585d36dd1bedf1bb8c0ee2ae2b15f
Francisco Abomo <f.mba1994@gmail.com>
2026-02-17 02:12:31 -0500

    Bump to versionCode 12 to retry build

--- app.config.js
+++ app.config.js
@@ -23,7 +23,7 @@ export default ({ config }) => ({
     },
     edgeToEdgeEnabled: true,
     package: "com.francisco1953.egwalletmobile",
-    versionCode: 10
+    versionCode: 12
   },
   web: {
     favicon: "./assets/favicon.png"
